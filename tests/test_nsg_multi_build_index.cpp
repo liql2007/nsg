dd5516dbb86fd5de6c039b646e896a61c28ab4df
qianling.liql <qianling.liql@alibaba-inc.com>
2021-01-04 18:00:02 +0800

    remove useless code

--- tests/test_nsg_multi_build_index.cpp
+++ tests/test_nsg_multi_build_index.cpp
@@ -11,69 +11,6 @@
 
 namespace {
 
-Partitions partitionData(const char* dataPath, const char* kMeansCentroidsPath,
-                         const char* multiIndexPath) {
-  std::cout << "** Partition Vectors" << std::endl;
-  float* vecData = nullptr;
-  unsigned pointNum, dim;
-  load_data(dataPath, vecData, pointNum, dim);
-  std::unique_ptr<float[]> vecHolder(vecData);
-
-  float* centroidsData = nullptr;
-  unsigned partNum, centroidDim;
-  load_data(kMeansCentroidsPath, centroidsData, partNum, centroidDim);
-  std::unique_ptr<float[]> centroidHolder(centroidsData);
-  assert(partNum > 1);
-  assert(centroidDim == dim);
-
-  Partitions parts = Partitions::create(multiIndexPath, partNum);
-  parts.dim = dim;
-  parts.totalVecNum = pointNum;
-
-  std::vector<std::unique_ptr<std::ofstream>> docStreams;
-  std::vector<std::unique_ptr<std::ofstream>> idStreams;
-  docStreams.reserve(partNum);
-  idStreams.reserve(partNum);
-  for (const auto& part : parts.partInfos) {
-    auto docStream = std::make_unique<std::ofstream>(
-      part.docPath.c_str(), std::ios::binary | std::ios::out);
-    docStreams.push_back(std::move(docStream));
-    auto idStream = std::make_unique<std::ofstream>(
-      part.idPath.c_str(), std::ios::binary | std::ios::out);
-    idStreams.push_back(std::move(idStream));
-  }
-
-  auto addDocFun = [&](unsigned id, const float* vec, unsigned globalId) {
-    docStreams[id]->write((const char*)&dim, sizeof(unsigned));
-    docStreams[id]->write((const char*)vec, sizeof(float) * dim);
-    idStreams[id]->write((const char*)&globalId, sizeof(unsigned));
-    ++parts.partInfos[id].vecNum;
-  };
-
-  faiss::IndexFlatL2 classifyIndex(dim);
-  classifyIndex.add(partNum, centroidsData);
-  const constexpr unsigned ProjectClusterNum = 2;
-  const constexpr unsigned BatchNum = 4096;
-  faiss::Index::idx_t label[ProjectClusterNum * BatchNum];
-  float dis[ProjectClusterNum * BatchNum];
-  for (unsigned gid = 0; gid < pointNum; gid += BatchNum) {
-    auto vecs = vecData + gid * dim;
-    auto realBatchNum = std::min(BatchNum, pointNum - gid);
-    classifyIndex.search(realBatchNum, vecs, ProjectClusterNum, &dis[0], &label[0]);
-    for (unsigned vi = 0; vi < realBatchNum; ++vi) {
-      auto vec = vecs + vi * dim;
-      for (unsigned k = 0; k < ProjectClusterNum; ++k) {
-        addDocFun(label[vi * ProjectClusterNum + k], vec, gid + vi);
-      }
-    }
-  }
-  for (unsigned i = 0; i < parts.partInfos.size(); ++i) {
-    docStreams[i]->close();
-    idStreams[i]->close();
-  }
-  return parts;
-}
-
 void buildKNN(const PartInfo& part, const char* knnBuildCmd) {
   char buildCmd[1024];
   sprintf(buildCmd, knnBuildCmd, part.docPath.c_str(), part.knnPath.c_str());
