f185d0fa3fd1af3f65f33cf78f4ced4abdd4b429
qianling.liql <qianling.liql@alibaba-inc.com>
2021-01-04 17:39:06 +0800

    overlapping partition to be a binary

--- tests/test_create_overlapping_shards.cpp
+++ tests/test_create_overlapping_shards.cpp
@@ -9,10 +9,17 @@
 #include <faiss/IndexFlat.h>
 #include <efanna2e/test_helper.h>
 
-namespace {
+int main(int argc, char** argv) {
+  if (argc != 4) {
+    std::cout << argv[0]
+              << " data_path kMeans_centroids_path multi_index_dir"
+              << std::endl;
+    exit(-1);
+  }
+  auto dataPath = argv[1];
+  auto kMeansCentroidsPath = argv[2];
+  auto multiIndexPath = argv[3];
 
-Partitions partitionData(const char* dataPath, const char* kMeansCentroidsPath,
-                         const char* multiIndexPath) {
   std::cout << "** Partition Vectors" << std::endl;
   float* vecData = nullptr;
   unsigned pointNum, dim;
@@ -67,56 +74,13 @@ Partitions partitionData(const char* dataPath, const char* kMeansCentroidsPath,
       }
     }
   }
+  parts.serialize();
+
   for (unsigned i = 0; i < parts.partInfos.size(); ++i) {
     docStreams[i]->close();
     idStreams[i]->close();
-  }
-  return parts;
-}
-
-void buildKNN(const PartInfo& part, const char* knnBuildCmd) {
-  char buildCmd[1024];
-  sprintf(buildCmd, knnBuildCmd, part.docPath.c_str(), part.knnPath.c_str());
-  std::cout << buildCmd << std::endl;
-  if (system(buildCmd) != 0) {
-    std::cerr << "build knn failed" << std::endl;
-    exit(-1);
-  }
-}
-
-}
-
-int main(int argc, char** argv) {
-  if (argc != 6) {
-    std::cout << argv[0]
-              << " multi_index_dir knn_build_cmd L R C"
-              << std::endl;
-    exit(-1);
-  }
-  auto multi_index_dir = argv[1];
-  auto knn_build_cmd = argv[2];
-  unsigned L = (unsigned)atoi(argv[3]);
-  unsigned R = (unsigned)atoi(argv[4]);
-  unsigned C = (unsigned)atoi(argv[5]);
-  std::cout << "L: " << L << std::endl;
-  std::cout << "R: " << R << std::endl;
-  std::cout << "C: " << C << std::endl;
-  std::cout << "KNN build cmd: " << knn_build_cmd << std::endl;
-
-#define OnlyBuildNSG 0
-
-  Partitions parts;
-  parts.deserialize(multi_index_dir);
-
-  double totalNsgBuildTime = 0;
-  for (unsigned i = 0; i < parts.partInfos.size(); ++i) {
     std::cout << "** Partition: " << i + 1 << std::endl;
-    const auto& part = parts.partInfos[i];
-
-#if OnlyBuildNSG == 0
-    std::cout << "Build KNN" << std::endl;
-    buildKNN(part, knn_build_cmd);
-#endif
+    const auto &part = parts.partInfos[i];
 
     float* vecData = NULL;
     unsigned pointNum, dimI;
@@ -124,25 +88,11 @@ int main(int argc, char** argv) {
     assert(dimI == parts.dim);
     std::unique_ptr<float[]>holder(vecData);
     std::cout << "vector num: " << pointNum / 10000.0 << "W" << std::endl;
-
-    std::cout << "Build NSG" << std::endl;
-    efanna2e::IndexNSG index(parts.dim, pointNum, efanna2e::L2, nullptr);
-    auto s = std::chrono::high_resolution_clock::now();
-    efanna2e::Parameters paras;
-    paras.Set<unsigned>("L", L);
-    paras.Set<unsigned>("R", R);
-    paras.Set<unsigned>("C", C);
-    paras.Set<std::string>("nn_graph_path", part.knnPath);
-
-    index.Build(pointNum, vecData, paras);
-    auto e = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff = e - s;
-    std::cout << "build time: " << diff.count() << "\n";
-    totalNsgBuildTime += diff.count();
-    index.Save(part.nsgPath.c_str());
+    std::cout << "Create Ground Truth" << std::endl;
+    GroundTruth::createPartGroundTruth(
+      part.queryPath.c_str(), part.groundTruthPath.c_str(),
+      vecData, pointNum, dimI, 100, 100);
   }
-  std::cout << "Part(" << parts.partInfos.size() << ") NSG avg build time: "
-            << totalNsgBuildTime / parts.partInfos.size() << std::endl;
-  std::cout << "NSG total build time: " << totalNsgBuildTime << std::endl;
+
   return 0;
 }
